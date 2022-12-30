c9e160a2def5f54d2b6f6ef13c46798260b38627
bharavsai15 <bhargavcme123@gmail.com>
2022-12-30 18:04:16 +0530

    first1

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -11,5 +11,26 @@ public class StudentAccount extends BankAccount{
         this.setMinBalance(0);
     }
 
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 
+    @Override
+    public double getBalance() {
+        return super.getBalance();
+    }
+
+    @Override
+    public double getMinBalance() {
+        return super.getMinBalance();
+    }
+
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
